4ea0a637e916fe9760c6369bdc0714055d5b33e0
dkjnna <faridfadilah70@gmail.com>
2024-08-28 19:42:35 +0700

    Membuat halaman home dan navbar

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<nav class="nav">
+    <div class="menu-icon"></div>
+    <img class="logo" src="asset/img/logo.png" alt="" srcset="">
+    <ul class="ul-menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Ticket</a></li>
+        <li><a href="#">Laayout</a></li>
+        <li><a href="#">Gallery</a></li>
+    </ul>
+    <img class="user" src="asset/img.user.png" alt="" srcset="">
+</nav>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="asset/css/main.css">
+</head>
+<body>
+    <div class="container">
+        <!-- Navbar -->
+        <?php require('includes/navbar.php') ?>
+
+        <!-- Home -->
+        <div class="home" id="home">
+            <img src="asset/img/bg.jfif" alt="" class="bg-home">
+            <div class="foto-home">
+                <img class="foto-bruno" src="asset/img/bruno.png" alt="">
+            </div>
+            <div class="content-home">
+                <h1 class="subtitle">Comming Soon</h1>
+                <h1 class="title">Bruno On Stage</h1>
+                <h3 class="location">JAKARTA INTERNATIONAL STADIUM</h3>
+                <div class="informasi">
+                    <p>Tour for the Latest Album - '24K Magic'.</p>
+                    <p>Sunday, September 15, 2024</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- ABOUT -->
+
+
+        <!-- TICKET -->
+
+
+        <!-- LAYOT -->
+
+        <!-- FOOTER -->
+
+        
+    </div>
+</body>
+</html>
